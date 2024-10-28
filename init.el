43e02a8acd04b487076c14756d69315b147f63a7
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-10-28 09:59:32 -0600

    smarter backups and nameing

--- init.el
+++ init.el
@@ -1,45 +1,5 @@
-;; _-_-_-_-_-_-_-_-_-_-_-_-_-EMACS CUSTOM VARS-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-;; we want vim C-u
-(setq evil-want-C-u-scroll t
-    visible-bell t
-    neo-smart-open t
-    completion-auto-help t
-    version-control t     ;; Use version numbers for backups.
-    kept-new-versions 10  ;; Number of newest versions to keep.
-    kept-old-versions 0   ;; Number of oldest versions to keep.
-    delete-old-versions t ;; Don't ask to delete excess backup versions.
-    backup-by-copying t   ;; Copy all files, don't rename them.
-    vc-make-backup-files t)
-
-;; add nrv custom dir
-(add-to-list 'load-path "~/.config/emacs/nrv")
-
-;; declare all your packages
-(setq my-packages
-      '(evil
-    evil-dvorak
-	evil-leader
-	neotree
-	gnu-elpa-keyring-update
-	)
-  )
-      
-;; _-_-_-_-_-_-_-_-_-_-_-_-_-EMACS CUSTOM STUFF_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(evil-leader evil-dvorak neotree gnu-elpa-keyring-update evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
+;; add custom dir to mf load-path
+(add-to-list 'load-path "~/.config/emacs/nrv" )
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -52,34 +12,79 @@
 
 (eval-when-compile
   (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
+ '(custom-safe-themes
+   '("3f75d4633820090be31d1f91fa1e33427b5dc09235efa189157592c822d1843a" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   '(neotree spacemacs-theme ace-window gnu-elpa-keyring-update evil-leader evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-;; _-_-_-_-_-_-_-_-_-_-_-_-_MAKE SURE ALL PACKAGES INSTALLED-_-_-_-_-_-_-_-
-(dolist (pkg my-packages)
-  (unless (package-installed-p pkg)
-    (package-install pkg)))
-;; _-_-_-_-_-_-_-_-_-_-_-_-_NRV EVIL-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-(require 'nrv-evil)
-;; _-_-_-_-_-_-_-_-_-_-_-_-_Commands-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-(defalias 'up 'package-refresh-contents)
-;; _-_-_-_-_-_-_-_-_-_-_-_-_Backups Start_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-;; Default and per-save backups go here:
-(setq backup-directory-alist '(("" . "~/.emacs_backups/backup/per-save")))
+;; easy change buffer with f keys (and close other win)
+(global-set-key (kbd "<f1>") 'previous-buffer)
+(global-set-key (kbd "<f2>") 'next-buffer)
+(global-set-key (kbd "<f3>") 'neotree-toggle)
+(global-set-key (kbd "<f4>") 'delete-other-windows )
+;; _-_-_-_-_-_-_-_-_-_-_-_-_-setq var's_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+;; we want vim C-u
+(setq evil-want-C-u-scroll t)
+(setq neo-smart-open t)
+(setq completion-auto-help t)
+;; _-_-_-_-_-_-_-_-_-_-_-_-_evil-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+;; evil bah-ha-ha
+(require 'evil)
+;; leader for emacs
+(require 'evil-leader) 
+(global-evil-leader-mode) ;; activate leader mode, must be done early
+;;my own l-dvorak bindings
+(require 'nrv-evil-dvorak)
+;; enable modded global dvorak mode 
+(global-evil-dvorak-mode 1)
+(evil-mode 1)
+;; set evil undo to one built into emacs 
+(evil-set-undo-system 'undo-redo)
+;; _-_-_-_-_-_-_-_-_-_-_-_-_-Keymaps-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+;; make c delete
+(define-key evil-normal-state-map (kbd "c") 'evil-delete)
+;; <leader>
+(evil-leader/set-leader "<SPC>") ;; set to space
+;; define leader mappings
+(evil-leader/set-key 
+  "w" 'save-buffer
+  "s" 'evil-window-split
+  "v" 'evil-window-vsplit
+  "k" 'kill-buffer
+  "q" 'evil-quit
+  "x" 'delete-window
+  "<SPC>" 'evil-window-prev
+)
 
-(defun force-backup-of-buffer ()
-  ;; Make a special "per session" backup at the first save of each
-  ;; emacs session.
-  (when (not buffer-backed-up)
-    ;; Override the default parameters for per-session backups.
-    (let ((backup-directory-alist '(("" . "~/.emacs_backups/per-session")))
-          (kept-new-versions 3))
-      (backup-buffer)))
-  ;; Make a "per save" backup on each save.  The first save results in
-  ;; both a per-session and a per-save backup, to keep the numbering
-  ;; of per-save backups consistent.
-  (let ((buffer-backed-up nil))
-    (backup-buffer)))
+(add-hook 'neotree-mode-hook
+              (lambda ()
+                (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "l") 'neotree-quick-look)
+                (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                (define-key evil-normal-state-local-map (kbd "<return>") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+                (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+                (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+                (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+                (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)
+                (define-key evil-normal-state-local-map (kbd "a") 'neotree-create-node)
+                (define-key evil-normal-state-local-map (kbd "r") 'neotree-delete-node)))
+;; _-_-_-_-_-_-_-_-_-_-_-_-_-Keymaps END_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ 
 
-(add-hook 'before-save-hook  'force-backup-of-buffer)
-
-;; _-_-_-_-_-_-_-_-_-_-_-_-_Backups End_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
