0ce7f2605423a3b72b1dcea9877f96f9691db6a7
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-01-23 09:46:35 -0600

    borked mess

--- init.el
+++ init.el
@@ -2,21 +2,25 @@
 ;;; commentary:
 ;; mostly keymaps that are mode specific
 
-(provide 'init)
 ;;; code:
-(when (< emacs-major-version 27)
-  (package-initialize))
+"Set up package managment and config use-package."
+;; add our lisp dir to load path
+(setq load-path (append  "/home/nrv/.config/emacs/lisp" load-path))
 
-;; add lisp dir to load-path
-(let ((default-directory "/home/nrv/.config/emacs/lisp" ))
-  (setq load-path
-        (append
-         (let ((load-path  (copy-sequence load-path))) ;; Shadow
-           (append
-            (copy-sequence (normal-top-level-add-to-load-path '(".")))
-            (normal-top-level-add-subdirs-to-load-path)))
-         load-path)))
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+(package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+;; package managment init done
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -39,6 +43,7 @@
  '(font-lock-variable-name-face ((t (:foreground "pale green" :weight extra-bold))))
  '(font-lock-variable-use-face ((t (:inherit font-lock-variable-name-face))))
  '(hl-line ((t (:extend t :background "grey18"))))
+ '(isearch ((t (:background "thistle" :foreground "dark red"))))
  '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "LightGoldenrod4"))))
  '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "DarkOrange4"))))
  '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "orchid"))))
@@ -48,16 +53,6 @@
  '(rainbow-delimiters-depth-7-face ((t (:inherit rainbow-delimiters-base-face :foreground "lawn green"))))
  '(rainbow-delimiters-depth-9-face ((t (:inherit rainbow-delimiters-base-face :foreground "DeepSkyBlue1"))))
  '(region ((t (:extend t :background "dark cyan")))))
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -67,13 +62,15 @@
  '(custom-safe-themes
    '("5f4b294798037c1abe4be3ee481897f533f2b088465c1f10f1ae8a0f297b4b1d" "ee0785c299c1d228ed30cf278aab82cf1fa05a2dc122e425044e758203f097d2" "3f75d4633820090be31d1f91fa1e33427b5dc09235efa189157592c822d1843a" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
  '(eglot-send-changes-idle-time 0.3)
+ '(evil-bigword "^ \11\15\12- / .")
  '(evil-emacs-state-modes
    '(dired-mode vterm-mode 5x5-mode archive-mode bbdb-mode biblio-selection-mode blackbox-mode bookmark-bmenu-mode bookmark-edit-annotation-mode browse-kill-ring-mode bs-mode bubbles-mode bzr-annotate-mode calc-mode cfw:calendar-mode completion-list-mode Custom-mode custom-theme-choose-mode debugger-mode delicious-search-mode desktop-menu-blist-mode desktop-menu-mode doc-view-mode dun-mode dvc-bookmarks-mode dvc-diff-mode dvc-info-buffer-mode dvc-log-buffer-mode dvc-revlist-mode dvc-revlog-mode dvc-status-mode dvc-tips-mode ediff-mode ediff-meta-mode efs-mode Electric-buffer-menu-mode emms-browser-mode emms-mark-mode emms-metaplaylist-mode emms-playlist-mode ess-help-mode etags-select-mode fj-mode gc-issues-mode gdb-breakpoints-mode gdb-disassembly-mode gdb-frames-mode gdb-locals-mode gdb-memory-mode gdb-registers-mode gdb-threads-mode gist-list-mode git-rebase-mode gnus-article-mode gnus-browse-mode gnus-group-mode gnus-server-mode gnus-summary-mode gomoku-mode google-maps-static-mode ibuffer-mode jde-javadoc-checker-report-mode magit-cherry-mode magit-diff-mode magit-log-mode magit-log-select-mode magit-popup-mode magit-popup-sequence-mode magit-process-mode magit-reflog-mode magit-refs-mode magit-revision-mode magit-stash-mode magit-stashes-mode magit-status-mode mh-folder-mode monky-mode mpuz-mode mu4e-main-mode mu4e-headers-mode mu4e-view-mode notmuch-hello-mode notmuch-search-mode notmuch-show-mode notmuch-tree-mode occur-mode org-agenda-mode package-menu-mode pdf-outline-buffer-mode pdf-view-mode proced-mode rcirc-mode rebase-mode recentf-dialog-mode reftex-select-bib-mode reftex-select-label-mode reftex-toc-mode sldb-mode slime-inspector-mode slime-thread-control-mode slime-xref-mode snake-mode solitaire-mode sr-buttons-mode sr-mode sr-tree-mode sr-virtual-mode tar-mode tetris-mode tla-annotate-mode tla-archive-list-mode tla-bconfig-mode tla-bookmarks-mode tla-branch-list-mode tla-browse-mode tla-category-list-mode tla-changelog-mode tla-follow-symlinks-mode tla-inventory-file-mode tla-inventory-mode tla-lint-mode tla-logs-mode tla-revision-list-mode tla-revlog-mode tla-tree-lint-mode tla-version-list-mode twittering-mode urlview-mode vc-annotate-mode vc-dir-mode vc-git-log-view-mode vc-hg-log-view-mode vc-svn-log-view-mode vm-mode vm-summary-mode w3m-mode wab-compilation-mode xgit-annotate-mode xgit-changelog-mode xgit-diff-mode xgit-revlog-mode xhg-annotate-mode xhg-log-mode xhg-mode xhg-mq-mode xhg-mq-sub-mode xhg-status-extra-mode))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(typescript-mode treesit-fallback rainbow-delimiters eglot yasnippet-classic-snippets markup markdown-mode company scroll-on-jump all-the-icons-gnus all-the-icons-nerd-fonts all-the-icons-dired all-the-icons-completion auto-rename-tag ac-html which-key yasnippet-snippets all-the-icons corfu jedi python-django vterm org-modern yasnippet centaur-tabs magit gnu-elpa-keyring-update evil reformatter))
+   '(eglot-booster evil-leader nrv-evil-dvorak flycheck-eglot flycheck typescript-mode treesit-fallback rainbow-delimiters eglot yasnippet-classic-snippets markup markdown-mode company scroll-on-jump all-the-icons-gnus all-the-icons-nerd-fonts all-the-icons-dired all-the-icons-completion auto-rename-tag ac-html which-key yasnippet-snippets all-the-icons corfu jedi python-django vterm org-modern yasnippet centaur-tabs magit gnu-elpa-keyring-update evil reformatter))
  '(package-vc-selected-packages
-   '((treesit-fallback :vc-backend Git :url "https://github.com/renzmann/treesit-fallback.git")))
+   '((eglot-booster :vc-backend Git :url "https://github.com/jdtsmith/eglot-booster")
+     (treesit-fallback :vc-backend Git :url "https://github.com/renzmann/treesit-fallback.git")))
  '(text-mode-hook
    '(turn-on-flyspell yas-minor-mode-on text-mode-hook-identify))
  '(tool-bar-mode nil))
@@ -85,14 +82,29 @@
 (savehist-mode) ;; save history
 (transient-mark-mode 1)  ;; selection highlighting
 (which-function-mode 1)  ;; tell which func.
-;; remove the legacy hook from flymake
+;; remove flymake
+(flymake-mode -1)
+
 ;;_-_-_-_-_-_-_-_-_-_-_-_-_-Packages_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-;; treesit.el an intelligent way to choose between a default mode, such as python-mode, and it’s tree-sitter enhanced version, python-ts-mode, automatically.
+
+;; treesit.el is an intelligent way to choose between a default mode, such as python-mode, and it’s tree-sitter enhanced version, python-ts-mode, automatically.
+
 (use-package treesit-auto
-  :demand t
   :ensure t
   :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode))
+
+(use-package flycheck-eglot
+  :ensure t
+  :after (flycheck eglot)
+  :config
+  (global-flycheck-eglot-mode t))
 
 (use-package rainbow-delimiters
   :load-path "lisp"
@@ -108,6 +120,7 @@
   ;; set evil undo to one built into emacs
   (evil-set-undo-system 'undo-redo)
   (use-package nrv-evil-dvorak
+    :load-path "lisp"
     :demand t
     :config
     (global-evil-dvorak-mode 1))
@@ -135,7 +148,6 @@
 ;; --- emacs lsp ---
 (use-package eglot
   :ensure t
-  :defer t
   :bind
   (("C-c b" . xref-go-back)
    ("C-c d" . xref-find-definitions)
@@ -145,16 +157,20 @@
   (js-mode . eglot-ensure)
   (typescript-ts-mode . eglot-ensure))
 
+;; ---- preformance enhancing drugs ---
+;;(use-package eglot-booster
+	;;:after eglot
+	;;:config	(eglot-booster-mode))
+
 (use-package centaur-tabs
   :ensure t
   :demand t
   :config
   (centaur-tabs-mode t)
-  (centaur-tabs-headline-match)
   :bind
   (
    :map centaur-tabs-mode-map
-        ("M-[" . centaur-tabs-backward)
+   ("M-[" . centaur-tabs-backward)
    ("M-]" . centaur-tabs-forward)
    ("M-{" . centaur-tabs-move-current-tab-to-left)
    ("M-}" .  centaur-tabs-move-current-tab-to-right)
@@ -171,13 +187,16 @@
 ;; display possible keyboard shortcuts
 (use-package which-key
   :ensure t
+  :defer t
   :config
   (which-key-mode))
 
 
 ;; corfu autocomplete ui
 (use-package corfu
-  :custom
+  :defer t
+  :ensure t
+  :config
   (corfu-auto t)
   (corfu-cycle t)  ;; Enable cycling
   (global-corfu-mode)
@@ -187,9 +206,8 @@
    ("<f6>" . corfu-next)
    ("<f7>" . corfu-previous)
    ("<f8>" . corfu-quit-no-match)
+   )
   )
-  :ensure t)
-
 ;; all the icons - icons in text
 ;; make sure to M-x: all-the-icons-install-fonts
 (use-package all-the-icons
@@ -211,8 +229,8 @@
    ("C-c C-g b" . #'magit-branch)
    ("C-c C-g a" . #'magit-stage-buffer-file)
    ("C-c C-g s" . #'magit-status-quick)
+   )
   )
-)
 
 (use-package typescript-mode
   :ensure t
@@ -220,6 +238,15 @@
   :config
   (setq typescript-indent-level 2))
 
+
+;;_-_-_-_-_-_-_-_-_-_-_-_-_elisp I found/require_-_-_-_-_-_-_-_-_-_-_-_-_-_
+;; the first require is squiggly no matter
+;; mode hooks
+(require 'nrv-modes) ;; modular af
+;; org
+(require 'org)
+;; my own custom stuff
+(require 'nrv-vterm)
 
 ;;_-_-_-_-_-_-_-_-_-_-_-_-_-setq var's_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 ;; Use spaces not tabs
@@ -235,22 +262,17 @@
  mouse-wheel-scroll-amount '(0.07)
  mouse-wheel-progressive-speed nil
 
- ;; JEDI AUTO complete
- jedi:complete-on-dot t
+ ;; AUTO complete
  completion-auto-help t
  completion-cycle-threshold nil ;; don't cycle completions
 
  ;; history/backup
- savehist-file "~/.config/emacs/backups/emacs_histfile"
  version-control t     ;; Use version numbers for backups.
  kept-new-versions 10  ;; Number of newest versions to keep.
  kept-old-versions 0   ;; Number of oldest versions to keep.
  delete-old-versions t ;; Don't ask to delete excess backup versions.
  backup-by-copying t   ;; Copy all files, don't rename them.
 
- ;; debug/backtrace
- debug-on-error t
- backtrace-depth 50
 
  ;; Revert/reload Dired and other buffers on filesystem change
  global-auto-revert-non-file-buffers t
@@ -271,48 +293,31 @@
  ;; org mode
  org-image-actual-width nil)
 
+
 ;;_-_-_-_-_-_-_-_-_-_-_-_-_-Global Keymaps-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 ;; restart emacs
 (global-set-key (kbd "C-M-r") 'restart-emacs)
-;; scroll on jump
-(global-set-key (kbd "<C-M-next>")  'diff-hl-next-hunk)
-(global-set-key (kbd "<C-M-prior>") 'diff-hl-previous-hunk)
 ;; alt - l (lisp) eval buffer
 (global-set-key (kbd "M-l") 'eval-buffer)
 ;; changing buffers
 ;; f9 vterm
-(global-set-key (kbd "<f9>") 'vterm)
 (global-set-key (kbd "<f1>") 'centaur-tabs-forward-group)
 (global-set-key (kbd "<f1>") 'centaur-tabs-backward-group)
 (global-set-key (kbd "A-[") 'centaur-tabs-backward)
 (global-set-key (kbd "A-]") 'centaur-tabs-forward)
 (global-set-key (kbd "<f9>") 'vterm)
 
-;;_-_-_-_-_-_-_-_-_-_-_-_-_elisp I found/require_-_-_-_-_-_-_-_-_-_-_-_-_-_
-;; the first require is squiggly no matter
-;; mode hooks
-(require 'nrv-modes) ;; modular af
-;; org
-(require 'org)
-;; my own custom stuff
-(require 'nrv-vterm)
-
 ;;_-_-_-_-_-_-_-_-_-_-_-_-_-Hooks_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 ;; remove trailing whitespace before saving and make backup
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
-(add-hook 'before-save-hook  'force-backup-of-buffer)
-(defun css-setup ()
-  "Setup Emacs for css editing."
-  (require 'evil)
-  (setq tab-width 2
-        evil-shift-width 2)
-)
+(add-hook 'before-save-hook  #'force-backup-of-buffer)
+
 
 ;; the prepare-XXX are defined in nrv-modes
 (add-hook 'prog-mode-hook #'prepare-prog)
 (add-hook 'python-mode-hook #'prepare-python)
 (add-hook 'dired-mode-hook #'prepare-dired)
-(add-hook 'css-mode-hook #'css-setup)
+(add-hook 'css-mode-hook #'prepaire-css)
 
 ;;_-_-_-_-_-_-_-_-_-_-_-_-_-Aliases_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 (defalias 'up 'package-refresh-contents)
@@ -336,4 +341,7 @@
 
 
 (provide 'init.el)
+;;; Local Variables:
+;; byte-compile-warnings: (not free-vars yas-snippet-dir)
+;; End:
 ;;; init.el ends here
--- lisp/nrv-modes.el
+++ lisp/nrv-modes.el
@@ -3,6 +3,15 @@
 ;; mostly keymaps that are mode specific
 
 ;;; code:
+
+(defvar python-tab-width 4 "Tab width for python source files.")
+(defvar java-tab-width 4 "Tab width for java source files.")
+(defvar css-tab-width 2 "Tab width for css source files.")
+(defvar js-tab-width 4 "Tab width for js source files.")
+(defvar html-tab-width 2 "Tab width for html files.")
+(defvar prog-tab-width 4 "Tab width for general programming files.")
+(defvar lisp-tab-width 2 "Tab width for Lisp files.")
+
 (defun prepare-dired ()
   "Prepare Dired mode how I like it."
   (require 'dired)
@@ -24,31 +33,36 @@
   (define-key 'dired-ring-map (kbd "x") #'delete-window)
   (define-key 'dired-ring-map (kbd "k") #'kill-this-buffer)
  )
+;; ---- css mode ----
+(defun prepaire-css ()
+  "Setup Emacs for css editing."
+  (setq tab-width css-tab-width
+        evil-shift-width 2)
+)
 ;; ---- python mode ----
 (defun prepare-python ()
   "Prepare to edit python code."
   (require 'python-isort)
-
-  ;; do not enable flymake on temp. buffers
-  (add-hook 'python-mode-hook
-            (lambda ()
-	      (unless (eq buffer-file-name nil) (flymake-mode 1))))
+  (setq tab-width python-tab-width)
   (add-hook 'python-mode-hook 'python-isort-on-save-mode)
 )
 ;; ---- programming mode ----
 (defun prepare-prog ()
   "Prepare to enter \='prog-mode'."
+  (setq tab-width prog-tab-width)
   ;; add lines to programming mode
   (add-hook 'prog-mode-hook 'display-line-numbers-mode)
   ;; colour define "(" pairs etc
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  ;; Flymake and codespell
-  (add-hook 'prog-mode-hook 'flymake-mode)
   ;; keybinds
   (define-key prog-mode-map (kbd "C-C l") 'flymake-show-buffer-diagnostics)
   (define-key prog-mode-map (kbd "C-C n") 'flymake-goto-next-error)
 )
+
 ;; remove trailing whitespace before saving
 ;; hook for changing modes
 (provide 'nrv-modes)
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars dired-mode-map evil-shift-width)
+;; End:
 ;;; nrv-modes.el ends here
