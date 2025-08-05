96d8cff58367a16b1ed78e8781fec1d3481ef60e
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-05 16:57:43 -0600

    reverted to a working vterm-nrv

--- lisp/vterm-nrv.el
+++ lisp/vterm-nrv.el
@@ -3,33 +3,32 @@
 ;; better than nothing, as everything must be Emacs
 
 ;;; code:
-;; popup shell
-(require 'shell-pop)
-
-(defun prep-vterm/nrv ()
+(defun use-monospace ()
   "Switch the current buffer to a monospace font."
-  (face-remap-add-relative 'default '(:family "Monospace"))
-	(buffer-face-mode t)
-	(text-scale-decrease 1)
-	(use-monospace)
-  (define-key vterm-mode-map (kbd "M-[")  #'centaur-tabs-backward)
-  (define-key vterm-mode-map (kbd "M-]") #'centaur-tabs-forward)
-  (define-key vterm-mode-map (kbd "M-{") #'centaur-tabs-move-current-tab-to-left)
-  (define-key vterm-mode-map (kbd "M-}") #'centaur-tabs-move-current-tab-to-right)
-  (define-key vterm-mode-map (kbd "<f1>") #'centaur-tabs-backward-group)
-  (define-key vterm-mode-map (kbd "<f2>") #'centaur-tabs-forward-group)
-  (define-key vterm-mode-map (kbd "<f3>") #'neotree-toggle)
-  (define-key vterm-mode-map (kbd "<f4>") #'shell-pop)
-  (define-key vterm-mode-map (kbd "C-c C-w") #'other-window))
+  (face-remap-add-relative 'default '(:family "Monospace")))
 
 (use-package vterm
   :ensure t
-  :defer t
   :custom
   (vterm-always-compile t)
   :hook
-  (vterm-mode . (prep-vterm/nrv)))
+  (vterm-mode . (lambda ()
+		              (buffer-face-mode t)
+		              (text-scale-decrease 1)
+		              (use-monospace)
+                  (define-key vterm-mode-map (kbd "M-[")  #'centaur-tabs-backward)
+                  (define-key vterm-mode-map (kbd "M-]") #'centaur-tabs-forward)
+                  (define-key vterm-mode-map (kbd "M-{") #'centaur-tabs-move-current-tab-to-left)
+                  (define-key vterm-mode-map (kbd "M-}") #'centaur-tabs-move-current-tab-to-right)
+                  (define-key vterm-mode-map (kbd "<f1>") #'centaur-tabs-backward-group)
+                  (define-key vterm-mode-map (kbd "<f2>") #'centaur-tabs-forward-group)
+                  (define-key vterm-mode-map (kbd "<f3>") #'neotree-toggle)
+                  (define-key vterm-mode-map (kbd "<f4>") #'shell-pop))
+              )
+  )
 
+;; popup shell
+(require 'shell-pop)
 
 (provide 'vterm-nrv)
 ;;; vterm-nrv.el ends here
