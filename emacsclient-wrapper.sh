3e63966b41c47719edf32e992b12c80c754019dd
Nicolas Vaagen <nicolasvaagen@gmail.com>
2026-06-09 16:03:02 -0600

    format and fix nohup command

--- emacsclient-wrapper.sh
+++ emacsclient-wrapper.sh
@@ -3,38 +3,34 @@
 # Wrapper for emacsclient that only spawns two windows
 
 start_emacs_daemon() {
-    if [[ -S /run/user/$(id -u)/emacs/server ]]
-    then
-        echo "daemon is up"
+	if [[ -S /run/user/$(id -u)/emacs/server ]]; then
+		echo "daemon is up"
 	else
-		nohup /usr/bin/emacs --daemon & &>/dev/null
+		nohup /usr/bin/emacs --daemon >/dev/null 2>&1 &
 		echo "started daemon"
 	fi
 }
 
 use_emacsclient() {
-    # check for -nw
-    if [[ $1 == "-nw" ]]
-    then
-        emacsclient "$@"
-        return
-    else  # GUI Emacs
-        for file in $@
-        do
-            echo "(nrv/open-or-create-file-buffer \"$file\") - sent to Emacs"
-            emacsclient -e "(nrv/open-or-create-file-buffer \"$file\")"
-        done
-    fi
+	# check for -nw
+	if [[ $1 == "-nw" ]]; then
+		emacsclient "$@"
+		return
+	else # GUI Emacs
+		for file in $@; do
+			echo "(nrv/open-or-create-file-buffer \"$file\") - sent to Emacs"
+			emacsclient -e "(nrv/open-or-create-file-buffer \"$file\")"
+		done
+	fi
 
 	# Count existing frames
 	frames=$(emacsclient -e "(length (frame-list))")
-	if [[ "$frames" -lt 3 ]]
-    then
-        emacsclient -c &
-    else
-        emacsclient -e "(select-frame-set-input-focus (selected-frame))"
-        echo "Reused frame. frames >= 2."
-    fi
+	if [[ "$frames" -lt 3 ]]; then
+		emacsclient -c &
+	else
+		emacsclient -e "(select-frame-set-input-focus (selected-frame))"
+		echo "Reused frame. frames >= 2."
+	fi
 
 }
 
@@ -42,10 +38,9 @@ use_emacsclient() {
 start_emacs_daemon
 
 # Wait until the daemon is ready
-for i in $(seq 1 10)
-do
-    emacsclient -e "t" &>/dev/null && break
-    sleep 1
+for i in $(seq 1 10); do
+	emacsclient -e "t" &>/dev/null && break
+	sleep 1
 done
 
 use_emacsclient "$@"
